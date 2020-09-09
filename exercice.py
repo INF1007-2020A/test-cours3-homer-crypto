e808a2f1e8a26e24d319d008c043a9ce124fe09f
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-09 12:34:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 def capitaliser_pays(nom):
     # TODO completer la fonction
-    mots_miniscule = [and']
+    mots_miniscules = ['and']
     nom.capitalize()
     return nom
 
