415401337630488c3ed7e17d151b6d8d91faf27f
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-09-09 12:42:56 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,9 +4,7 @@ def capitaliser_pays(nom):
     # TODO completer la fonction
     mots_miniscules = ['and','et']
     nom.capitalize()
-    for i in range(mots_miniscules):
-        nom = nom.replace(mots_miniscules[i],mots_miniscules[i].lower())
-    return nom
+   
 
 
 if __name__ == '__main__':
